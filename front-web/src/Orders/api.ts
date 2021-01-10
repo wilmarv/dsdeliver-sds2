00e61a4f3050ba069fd750d83f5bc4785a726523
wilmar vitor fonseca <wilmarfonseca@gec.inatel.br>
2021-01-10 16:43:39 -0300

    Search order location

--- front-web/src/Orders/api.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-const API_URL = 'http://localhost:8090'
-export function fetchProducts(){
-    return axios(`${API_URL}/products`)
-}
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
-import { fetchProducts } from './api';
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './style.css';
-import { Product } from './types';
+import { OrderLocationdata, Product } from './types';
 function Orders(){
     const [products, setProducts] = useState<Product[]>([]);
-
-    
+    const [orderLocation,setOrderLocation] =useState<OrderLocationdata>();    
     useEffect(() =>{
         fetchProducts()
         .then(response =>setProducts(response.data))
@@ -18,6 +18,7 @@ function Orders(){
         <div className="orders-container">
             <StepsHeader/>
             <ProductsList products={products}/>
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
         </div>
     )
 }
